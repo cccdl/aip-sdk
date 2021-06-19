3c37f67207936e77b9e0289b797fad3ee2c7b4c6
dl_c <568078220@qq.com>
2021-06-19 18:46:06 +0800

    百度sdl aip包

--- src/Examples/apiFace.php
+++ src/Examples/apiFace.php
@@ -1,8 +1,3 @@
 <?php
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use cccdl\aip_sdk\AipFace;
-
-$api = new AipFace('24401562', '8ztH45IiFcG0TGobti41wlw1', 'ANIQTntcTi3clEbZXkvRgETf3Gk8CLFe');
-$res = $api->personIdmatch('陈德龙', '441701199206010019');
-var_dump($res);
